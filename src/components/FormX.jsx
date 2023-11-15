6ca842f9257860a90f7f120a841f9a4f7defd55f
Emil Kristensen <emil00y1@stud.kea.dk>
2023-11-15 15:33:11 +0100

    state changes

--- src/components/FormX.jsx
+++ src/components/FormX.jsx
@@ -1,16 +1,42 @@
-import { useState } from "react";
-import { Checkbox } from "antd";
+import React, { useState } from "react";
+import { Checkbox, Radio } from "antd";
+
 export default function FormX() {
   const [quiz, updateQuiz] = useState(1);
 
+  function updater() {
+    updateQuiz((o) => o + 1);
+  }
+
+  const content = () => {
+    if (quiz === 1) {
+      return (
+        <>
+          <Checkbox>Lotion</Checkbox>
+          <Checkbox>Olie</Checkbox>
+          <Checkbox>Serum</Checkbox>
+          <Checkbox>Mousse</Checkbox>
+        </>
+      );
+    } else if (quiz === 2) {
+      return (
+        <>
+          <Radio.Group buttonStyle="solid">
+            <Radio.Button value="a">Nyheder</Radio.Button>
+            <Radio.Button value="b">Bestseller</Radio.Button>
+            <Radio.Button value="c">Beijing</Radio.Button>
+            <Radio.Button value="d">Chengdu</Radio.Button>
+          </Radio.Group>
+        </>
+      );
+    } else {
+      return <></>;
+    }
+  };
+
   return (
     <>
-      <form>
-        <Checkbox>Lotion</Checkbox>
-        <Checkbox>Olie</Checkbox>
-        <Checkbox>Serum</Checkbox>
-        <Checkbox>Mousse</Checkbox>
-      </form>
+      <form>{content()}</form>
     </>
   );
 }
--- src/components/ModalX.jsx
+++ src/components/ModalX.jsx
@@ -40,27 +40,40 @@ export default function ModalX() {
       </Button>
       <Modal
         open={open}
-        onCancel={decline}
-        onOk={increase}
-        okText={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-arrow-right-short"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
-            />
-          </svg>
-        }
-        cancelText="Forrige"
-        okType="text"
-        cancelType="text"
         title="Title"
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" type="text" onClick={decline}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-arrow-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+              />
+            </svg>
+          </Button>,
+          <Button key="submit" type="text" loading={loading} onClick={increase}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-arrow-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+              />
+            </svg>
+          </Button>,
+        ]}
       >
         <div
           style={{
@@ -69,7 +82,7 @@ export default function ModalX() {
         >
           <Progress percent={percent} />
         </div>
-        <FormX increase={increase}></FormX>
+        <FormX></FormX>
       </Modal>
     </>
   );

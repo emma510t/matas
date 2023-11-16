ac165480a877bc54c92351a184318e80de78e7a0
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-11-16 10:28:00 +0100

    .

--- src/components/FormX.jsx
+++ src/components/FormX.jsx
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
-import { Checkbox, Radio } from "antd";
+import { Checkbox, Radio, Button } from "antd";
 
 export default function FormX({ percent }) {
   const content = () => {
     if (percent === 0) {
       return (
         <>
-          <Checkbox>Lotion</Checkbox>
-          <Checkbox>Olie</Checkbox>
-          <Checkbox>Serum</Checkbox>
-          <Checkbox>Mousse</Checkbox>
+          <Checkbox className=" text-base">Lotion</Checkbox>
+          <Checkbox className=" text-base">Olie</Checkbox>
+          <Checkbox className=" text-base">Serum</Checkbox>
+          <Checkbox className=" text-base">Mousse</Checkbox>
         </>
       );
     } else if (percent === 50) {
       return (
         <>
-          <Radio.Group buttonStyle="solid">
-            <Radio.Button value="a">Nyheder</Radio.Button>
-            <Radio.Button value="b">Bestseller</Radio.Button>
-            <Radio.Button value="c">Beijing</Radio.Button>
-            <Radio.Button value="d">Chengdu</Radio.Button>
-          </Radio.Group>
+          <div className="grid grid-cols-2 gap-1.5">
+            <Button className="flex justify-center items-center text-base" value="a">
+              Nyheder
+            </Button>
+            <Button className="flex justify-center items-center text-base" value="b">
+              Bestseller
+            </Button>
+            <Button className="flex justify-center items-center text-base" value="c">
+              Beijing
+            </Button>
+            <Button className="flex justify-center items-center text-base" value="d">
+              Chengdu
+            </Button>
+          </div>
         </>
       );
     } else {
@@ -30,7 +38,7 @@ export default function FormX({ percent }) {
 
   return (
     <>
-      <form>{content()}</form>
+      <form className="flex flex-col">{content()}</form>
     </>
   );
 }

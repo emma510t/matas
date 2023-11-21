2c487cf43e0db8437e33a2b3df902c8c71f34177
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-11-21 12:53:18 +0100

    fiks det emil

--- src/components/ProductCardX.jsx
+++ src/components/ProductCardX.jsx
@@ -1,14 +1,9 @@
 "use client";
 import { Card } from "antd";
 
-export default function ProductCardX({ children, name }) {
+export default function ProductCardX({ children, image }) {
   return (
-    <Card
-      className="rounded-lg"
-      hoverable
-      style={{ width: 240 }}
-      cover={<img alt="example" src="https://images.matas.dk/trs/w730//Assets_v3/600001-700000/653001-654000/653601-653700/653653/product_v1_x2.jpg" className="rounded-t-lg" id="rounded" />}
-    >
+    <Card className="rounded-lg" hoverable style={{ width: 240 }} cover={<img alt="example" src={image} className="rounded-t-lg" id="rounded" />}>
       {children}
     </Card>
   );

39cbed0d0ee37e7544de84e3b9c4e0b5c39897e1
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-11-21 11:05:48 +0100

    .

---
+++ src/components/ProductCardX.jsx
@@ -0,0 +1,10 @@
+"use client";
+import { Card } from "antd";
+
+export default function ProductCardX({ children, name }) {
+  return (
+    <Card className="rounded-lg" hoverable style={{ width: 240 }} cover={<img alt="example" src={name} className="rounded-t-lg" id="rounded" />}>
+      {children}
+    </Card>
+  );
+}

0980b373079a86f4b2d276a8741491786386496a
Emil Kristensen <emil00y1@stud.kea.dk>
2023-11-21 12:54:28 +0100

    things

--- src/components/CardX.jsx
+++ src/components/CardX.jsx
@@ -3,7 +3,14 @@ import { Card } from "antd";
 
 export default function CardX({ children, name }) {
   return (
-    <Card className="rounded-lg" hoverable style={{ width: 240 }} cover={<img alt="example" src={name} className="rounded-t-lg" id="rounded" />}>
+    <Card
+      className="rounded-lg"
+      hoverable
+      style={{ width: 240 }}
+      cover={
+        <img alt="example" src={name} className="rounded-t-lg" id="rounded" />
+      }
+    >
       {children}
     </Card>
   );

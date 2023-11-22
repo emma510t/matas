cb8d08db733232b4e9c90b9dc980678e98a7d352
Emma Hamdorf-Baunsgaard <emma510t@stud.kea.dk>
2023-11-22 15:17:42 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,17 @@
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { Menu, SubMenu, Item } from "burger-menu";
+import { Divider } from "antd";
 import "burger-menu/lib/index.css";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="header">
-      <svg
-        className="menu_btn"
-        onClick={() => setIsOpen(!isOpen)}
-        xmlns="http://www.w3.org/2000/svg"
-        height="30"
-        viewBox="0 -960 960 960"
-        width="30"
-      >
+      <svg className="menu_btn" onClick={() => setIsOpen(!isOpen)} xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 -960 960 960" width="30">
         <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
       </svg>
-      <Menu
-        className="burger-menu"
-        side="left"
-        isOpen={isOpen}
-        selectedKey={"entry"}
-        onClose={() => setIsOpen(false)}
-      >
+      <Menu className="burger-menu" side="left" isOpen={isOpen} selectedKey={"entry"} onClose={() => setIsOpen(false)}>
         <Item itemKey={"hår"} text={"Hår"}></Item>
         <Item itemKey={"makeup"} text={"Makeup"}></Item>
         <SubMenu title="Hudpleje">
@@ -32,17 +20,10 @@ function Header() {
           <Item itemKey={"krop"} text={"Kropspleje"}></Item>
           <Item itemKey={"gave"} text={"Gaveæsker"}></Item>
         </SubMenu>
-        <Item
-          icon={
-            <img
-              className="w-16"
-              src="/img/oekologisk-hudpleje.jpg"
-              alt="woman"
-            />
-          }
-          itemKey={"quiz"}
-          text={"Hudpleje: Tag testen"}
-        ></Item>
+        <Divider style={{ height: 1 }} className="bg-matas-blue" />
+        <Item icon={<img className="w-16 rounded" src="/img/oekologisk-hudpleje.jpg" alt="woman" />} itemKey={"quiz"} text={"Hudpleje: Tag testen"}>
+          <p>hejsa</p>
+        </Item>
       </Menu>
     </header>
   );

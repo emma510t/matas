af74e30bca435f49ab247de45ce9d71ad7ee9fa8
Emil Kristensen <emil00y1@stud.kea.dk>
2023-11-22 14:48:32 +0100

    npm install burger-menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,51 @@
 "use client";
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { Menu, SubMenu, Item } from "burger-menu";
+import "burger-menu/lib/index.css";
 
 function Header() {
-  return <header className="header"></header>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <header className="header">
+      <svg
+        className="menu_btn"
+        onClick={() => setIsOpen(!isOpen)}
+        xmlns="http://www.w3.org/2000/svg"
+        height="30"
+        viewBox="0 -960 960 960"
+        width="30"
+      >
+        <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+      </svg>
+      <Menu
+        className="burger-menu"
+        side="left"
+        isOpen={isOpen}
+        selectedKey={"entry"}
+        onClose={() => setIsOpen(false)}
+      >
+        <Item itemKey={"hår"} text={"Hår"}></Item>
+        <Item itemKey={"makeup"} text={"Makeup"}></Item>
+        <SubMenu title="Hudpleje">
+          <Item itemKey={"ansigt"} text={"Ansigtspleje"}></Item>
+          <Item itemKey={"krop"} text={"Kropspleje"}></Item>
+          <Item itemKey={"gave"} text={"Gaveæsker"}></Item>
+        </SubMenu>
+        <Item
+          icon={
+            <img
+              className="w-16"
+              src="/img/oekologisk-hudpleje.jpg"
+              alt="woman"
+            />
+          }
+          itemKey={"quiz"}
+          text={"Hudpleje: Tag testen"}
+        ></Item>
+      </Menu>
+    </header>
+  );
 }
 
 export default Header;
